b69f92408a3a3ea66c248acd3202b2b1568fcf5b
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-07-18 13:26:57 +0530

    added Method overloading

--- Functions or Methods/src/Method_Overloading.java
+++ Functions or Methods/src/Method_Overloading.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Method_Overloading {
+public class Method_Overloading {
+    public static void main(String[] args) {
+        func(1);
+        func("arnb");
+    }
+
+    static void func(int a ){
+        System.out.println(a);
+    }
+    static void func(String name){
+        System.out.println(name);
+    }
+
+
 }
