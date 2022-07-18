83c91ee4cbf729b400b603e1633d9783975bc6c8
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-07-18 13:27:15 +0530

    added Variable Length Arguments

--- Functions or Methods/src/Variable_Length_Arguments.java
+++ Functions or Methods/src/Variable_Length_Arguments.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Variable_Length_Arguments {
+import java.util.Arrays;
+
+public class Variable_Length_Arguments {
+    public static void main(String[] args) {
+        fun(2,3,4,5,6,76);
+        fun(); // empty array
+        fun2(1,2,"a","b","v");
+    }
+    static void fun(int ...v){
+        // interally int ...v this stores an array of integer
+        System.out.println(v); // showing memory location
+        System.out.println(Arrays.toString(v)); // getting the integers
+    }
+    static void fun2(int a , int b , String ...v){
+        // combination of different araguments
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(Arrays.toString(v));
+    }
 }
