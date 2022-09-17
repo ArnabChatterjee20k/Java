758f9a67bbc5e67f295489061441eb85d4491397
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-09-17 10:56:24 +0530

    modified comments of cyclic sort

--- Algorithm/Sorting/src/CyclicSort/CyclicSort.java
+++ Algorithm/Sorting/src/CyclicSort/CyclicSort.java
@@ -12,7 +12,7 @@ public class CyclicSort{
 
     static void cyclicSort(int[] arr){
         // if the range of numbers is from 1 to N(length of arr) use => correctIndex = arr[i]-1 
-        // if the range of numbers is from 0 to N(length of arr) use => correctIndex = i
+        // if the range of numbers is from 0 to N(length of arr) use => correctIndex = arr[i]
         int i = 0;
         while(i<arr.length){
             int correctIndex = arr[i]-1;
